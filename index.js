334f11682744ab024de9491f90a8931fe5023375
julydancer <brunolisboalima167@gmail.com>
2026-01-20 23:08:36 -0300

    Projeto inicial com .gitignore e .env

---
+++ index.js
@@ -0,0 +1,42 @@
+require("dotenv").config(); 
+const { Client, GatewayIntentBits } = require("discord.js");
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+  ],
+});
+
+const ID_GLAUBER = "413368679313440769";
+const ID_TAKESHI = "334331130927120386";
+const ID_ELVI = "1163232722538483812";
+const ID_ANA = "127792441238487040"
+
+client.on("messageCreate", (message) => {
+  if (message.author.bot) return;
+
+  const texto = message.content.toLowerCase().trim();
+
+  if (
+    message.author.id === ID_GLAUBER &&
+    texto === "bom dia"
+  ) {
+    message.reply("sucumba glauber");
+  }
+  else if(
+    message.author.id === ID_TAKESHI &&
+    texto === "saas"
+  ) {
+    message.reply("https://tenor.com/view/eddie-zato-zato-guilty-gear-eddie-clap-rde-gif-13918902029952866835")
+  }
+  else if(
+    message.author.id === ID_ELVI
+  ){
+    message.reply("VAI SE FUDER ELVI VIRA GENTE")
+  }
+});
+  
+
+client.login(TOKEN);
